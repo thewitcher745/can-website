444a1d0377ed9329ba7b54144cea190a4c34c969
thewitcher745 <thewitcher745@gmail.com>
2025-01-29 18:08:23 +0330

    FEAT: Added landing page button

--- src/pages/sections/Landing/Landing.tsx
+++ src/pages/sections/Landing/Landing.tsx
@@ -1,10 +1,9 @@
-// src/pages/sections/Landing/Landing.tsx
 import './Landing.scss';
 import {motion} from 'framer-motion';
 
 const Landing = () => {
     return (
-        <section id="landing" className="flex">
+        <section id="landing" className="flex py-20">
             <div id="landing-bg"/>
             <div id="bg-overlay"/>
             <div className="flex justify-start w-full h-full items-baseline">
@@ -15,7 +14,7 @@ const Landing = () => {
                         animate={{opacity: 1, translateX: 0}}
                         transition={{duration: 1, ease: "easeInOut"}}
                     >
-                        CAN trading solutions
+                        Elevate your trading
                     </motion.p>
                     <div className="flex flex-col px-2 justify-center font-light text-3xl md:text-4xl">
                         <motion.p
@@ -32,21 +31,25 @@ const Landing = () => {
                             animate={{opacity: 1, translateX: 0}}
                             transition={{duration: 1, delay: 0.6, ease: "easeInOut"}}
                         >
-                            perfected with experience.
+                            directed by foresight.
                         </motion.p>
                     </div>
-                    <motion.button
-                        className="cursor-pointer bg-amber-500 border-1 text-gray-800 font-light text-xl px-6 py-4 mx-2 mt-10 rounded-md hover:bg-transparent hover:border-1 hover:border-gray-100 hover:text-gray-100"
+                    <motion.div
                         initial={{opacity: 0, translateX: -60}}
                         animate={{opacity: 1, translateX: 0}}
                         transition={{duration: 1, delay: 1, ease: "easeInOut"}}>
-                        Learn more
-                    </motion.button>
+                        <button
+                            className="cursor-pointer bg-amber-500 border-1 text-gray-800 font-light text-xl px-6 py-4 mx-2 mt-10 rounded-md hover:bg-transparent hover:border-1 hover:border-gray-100 hover:text-gray-100 transition duration-200">
+                            Learn more
+                        </button>
+                    </motion.div>
+
                 </div>
             </div>
 
         </section>
-    );
+    )
+        ;
 };
 
 export default Landing;
